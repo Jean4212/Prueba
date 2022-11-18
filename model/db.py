9ed153e3ecb477a02854509272674879db6982ca
Jean <jeanoi4212@gmail.com>
2022-11-18 07:36:24 -0500

    mejorando

--- model/db.py
+++ model/db.py
@@ -1,78 +1,55 @@
 
 from sqlite3 import connect
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String
 
 
-engine = create_engine("sqlite:///./data/db.db")
-session = sessionmaker(bind=engine)
-new_session = session()
-base = declarative_base()
+class Database:
 
-class Users(base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    user = Column(String(10), nullable=False, unique=True)
-    password = Column(String(10), nullable=False)
+    def __init__(self, path: str) -> None:
 
-    def __init__(self, user, password):
-        self.user = user
-        self.password = password
+        path_db = path       
+        
+        try:            
+            connect(path_db)
+            self.active = True
+            
+        except: 
+            self.active = False
+            
+               
+    def select(self, query:str, multiline: bool) -> None:
+        if self.active:
+            engine = connect()
+            session = engine.cursor()                
+            result = session.execute(query).fetchall()
 
-    #def __repr__(self):
-    #    return f"({self.user}, {self.password})"
+            return result
 
-    def __str__(self):
-        return self.user
+                 
+    
 
-
-
-base.metadata.create_all(engine)
-
-def create_user():    
-    new_user = Users("Jeancarlos55", "1234")
-    new_session.add(new_user)
-    new_session.commit()
-    print(new_user.id)
-
-def select_user(user):
-    query = f""" SELECT * FROM users WHERE user = "{user}" """
-    query = new_session.execute(query).fetchall()
-    #result = new_session.execute(query).fetchone()
-    #user = query[0]
-    #password = query[1]
-    for item in query:
-        print(item)
-    #print(query)
-    #print({"user": user, "password": password})
-
-#create_user()
-select_user("Jeancarlos12")
+# engine = create_engine("sqlite:///./data/db.db")
+# session = sessionmaker(bind=engine)
+# new_session = session()
+# base = declarative_base()
+aa = Database("./data/db.sqlite3")
 
 
 
-
-
-with engine.connect() as conn:
-    pass
 
 def __conexion(query: str):
     try:
-        path = "./data/db.db"
-        con = connect(path)   
-        cur = con.cursor()
-        cur.execute(query)
-        con.commit()
-        con.close()
-        return {"message": "registro exitoso"}
+        path = "./data/db.sqlite3"
+        engine = connect(path)
+        session = engine.cursor()
+        session.execute(query)
+        engine.commit()
+        engine.close()
+        return True
     except:      
-        return {"message": "ocurrio un error"}
+        return False
           
 def insert(query: str):
     return __conexion(query)
-
       
 def select(query: str):
     try:
@@ -85,8 +62,6 @@ def select(query: str):
         return data
     except:      
         return {"message": "ocurrio un error"}
-
-
 
 def update(query: str):
     __conexion(query, False, False)    

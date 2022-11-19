86c5e5917aaa3826f7aa99f5b9f5c6a1c58062c0
Jean <jeanoi4212@gmail.com>
2022-11-18 19:27:02 -0500

    mejorando

--- router/router.py
+++ router/router.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from model.db import select
 from schema.worker import User
 
 
@@ -13,28 +12,11 @@ templates = Jinja2Templates(directory="./templates")
 
 @router.get("/", response_class=HTMLResponse, status_code=200)
 async def index(request: Request):
-    query = f"""SELECT * FROM WORKERS"""
-    result = select(query)    
-    data = []
-    for worker in result:
-        mydict = {  "dni": worker[0],
-                    "nombre": worker[1]}
-        data.append(mydict)
-
-    return templates.TemplateResponse("index.html", {"request": request, "message": data})  
+    return templates.TemplateResponse("index.html", {"request": request, "message": "hellow"})  
    
 
 
 
-
-
-
-
-#@router.post("/user")
-#def create_worker(worker: Worker):
-#    query = f"""INSERT INTO WORKERS (DNI, NAME) VALUES ("{worker.dni}", "{worker.nombre}")"""
-#    result = insert(query)
-#    return result
 
 @router.post("/user")
 def user(user: User):
@@ -43,15 +25,3 @@ def user(user: User):
 
     return {"message": "Fail"}
 
-
-@router.get("/job", response_class=HTMLResponse, status_code=200)
-async def index(request: Request):
-    query = f"""SELECT * FROM WORKERS"""
-    result = select(query)    
-    data = []
-    for worker in result:
-        mydict = {  "dni": worker[0],
-                    "nombre": worker[1]}
-        data.append(mydict)
-
-    return templates.TemplateResponse("job.html", {"request": request, "message": data})  

394fd413327471a48b7bca279e7896aefe12dfea
Jean <jeanoi4212@gmail.com>
2022-11-22 19:29:57 -0500

    mejorando

--- router/router.py
+++ router/router.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-
-
-
+from schema.worker import User
 
 router = APIRouter()
 templates = Jinja2Templates(directory="./templates")
@@ -14,19 +12,13 @@ templates = Jinja2Templates(directory="./templates")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request}) 
 
-@router.post("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return "hola" #templates.TemplateResponse("index.html", {"request": request})  
+@router.post("/")
+async def home(user:User):
+    print(user)
+    if user.user == "jean" and user.password == "1234":       
+        return {"message": True}
+
+    return {"message": False}
 
 
 
-
-   
-@router.post("/login", response_class=HTMLResponse)
-async def uuuuu(request: Request, username: str = Form(...), password: str = Form(...)):
-    
-    if username == "jean" and password == "1234":
-        print("hola")
-        return templates.TemplateResponse("user.html", {"request": request})  
-    
-    return RedirectResponse("/")  
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-function myfunctionName(){
+function mifun(){
 
     const user = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -6,12 +6,22 @@ function myfunctionName(){
     let datos = {"user": user, "password": password};
     let datosJSON = JSON.stringify(datos);
 
-    fetch('/login', {
+    fetch('/', {
           method: "POST",         
           headers: {"Content-Type": "application/json"},
           body: datosJSON
     })
     .then(response => response.json())  // convertir a json
-    .then(json => console.log(json))    //imprimir los datos en la consola
+    .then(json => {
+      if (json.message == false){
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+        console.log("no ingresastes");
+    }
+    else{
+      console.log("ingresastes")
+    }
+    }
+    )    //imprimir los datos en la consola
     .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
   ;}

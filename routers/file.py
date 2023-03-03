9a2fc89fb7e8d798c090ed042bfff2c85a679def
Jean <jeanoi4212@gmail.com>
2023-03-03 11:04:26 -0500

    mejorando

--- routers/file.py
+++ routers/file.py
@@ -6,17 +6,17 @@ route_file = APIRouter()
 @route_file.post("/uploadfile")
 async def create_upload_file(file: UploadFile = File(...)):
 
-    file_location = path.join(getcwd() + "/static/" + file.filename)
+    file_location = path.join(getcwd() + "/static/uploads/" + file.filename)
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
    
     return {"success": True}
 
 @route_file.post("/uploadfiles")
-async def create_upload_file(files: list[UploadFile] = File(...)):
+async def create_upload_files(files: list[UploadFile] = File(...)):
 
     for file in files:
-        file_location = path.join(getcwd() + "/static/" + file.filename)
+        file_location = path.join(getcwd() + "/static/uploads/" + file.filename)
         with open(file_location, "wb+") as file_object:
             file_object.write(file.file.read())
 
--- static/prueba.js
+++ static/prueba.js
@@ -1,21 +1,21 @@
-function ValidaDatos(event) {      
+function ValidaDatos() {      
     const dni = document.getElementById("dni");
     const paterno = document.getElementById("paterno");
     const materno = document.getElementById("materno");
     const nombre = document.getElementById("nombre");
     const nacimiento = document.getElementById("nacimiento");
 
-    if (!dni.value){alert("registre el dni"); dni.focus(); return};
-    if (dni.value.length < 8){alert("DNI SON 8 DIGITOS"); dni.focus(); return};
-    if (!paterno.value){alert("registre el apellido paterno"); paterno.focus(); return};
-    if (paterno.value.length < 3){alert("paterno debe ser mayor a 3 DIGITOS"); paterno.focus(); return};
-    if (!materno.value){alert("registre el apellido materno"); materno.focus(); return};
-    if (materno.value.length < 3){alert("materno debe ser mayor a 3 DIGITOS"); materno.focus(); return};
-    if (!nombre.value){alert("registre el nombre"); nombre.focus(); return};
-    if (nombre.value.length < 3){alert("nombre debe ser mayor a 3 DIGITOS"); nombre.focus(); return};
-    if (!nacimiento.value){alert("registre la fecha de nacimiento"); nacimiento.focus(); return};    
+    if (!dni.value){Swal.fire({title:"Registra el DNI", didClose: () => {dni.focus()}}); return};
+    if (dni.value.length < 8){Swal.fire({title:"Registra correctamente el DNI", didClose: () => {dni.focus()}}); return};
+    if (!paterno.value){Swal.fire({title:"Registra el Apellido PATERNO", didClose: () => {paterno.focus()}}); return};
+    if (paterno.value.length < 3){Swal.fire({title:"Registra correctamente el Apellido PATERNO", didClose: () => {paterno.focus()}}); return};
+    if (!materno.value){Swal.fire({title:"Registra el Apellido MATERNO", didClose: () => {materno.focus()}}); return};
+    if (materno.value.length < 3){Swal.fire({title:"Registra correctamente el Apellido MATERNO", didClose: () => {materno.focus()}}); return};
+    if (!nombre.value){Swal.fire({title:"Registra el NOMBRE", didClose: () => {nombre.focus()}}); return};
+    if (nombre.value.length < 3){Swal.fire({title:"Registra correctamente el NOMBRE", didClose: () => {nombre.focus()}}); return};
+    if (!nacimiento.value){Swal.fire({title:"Registra la Fecha de NACIMIENTO", didClose: () => {nacimiento.focus()}}); return};
     let fechaNacimiento = parseInt(nacimiento.value.slice(0,4));
-    if (fechaNacimiento < 1950 || fechaNacimiento > 2050){alert("fecha fuera de rango"); nacimiento.focus(); return};
+    if (fechaNacimiento < 1950 || fechaNacimiento > 2050){Swal.fire({title:"Registra correctamente la Fecha de NACIMIENTO", didClose: () => {nacimiento.focus()}}); return};
     
     const check1 = document.getElementById("Checked1");   
 
@@ -33,11 +33,11 @@ function ValidaDatos(event) {
     const aportacion = document.getElementById("aportacion");
     const comision = document.getElementById("comision");
 
-    if (!ingreso.value){alert("registra la fecha de ingreso"); ingreso.focus(); return};
+    if (!ingreso.value){Swal.fire({title:"Registra la Fecha de INGRESO", didClose: () => {ingreso.focus()}}); return};
     let fechaIngreso = parseInt(ingreso.value.slice(0,4));
-    if (fechaIngreso < 1950 || fechaIngreso > 2050){alert("fecha de ingreso fuera de rango"); ingreso.focus(); return};
-    if (!planilla.value){alert("registre el sueldo en planilla"); planilla.focus(); return};  
-    if (aportacion.value && aportacion.value != "onp" && !comision.value){alert("seleccione la comision"); comision.focus(); return};    
+    if (fechaIngreso < 1950 || fechaIngreso > 2050){Swal.fire({title:"Registra correctamente la Fecha de INGRESO", didClose: () => {ingreso.focus()}}); return};
+    if (!planilla.value){Swal.fire({title:"Registra el Sueldo en PLANILLA", didClose: () => {planilla.focus()}}); return};
+    if (aportacion.value && aportacion.value != "onp" && !comision.value){Swal.fire({title:"Registra el Tipo de COMISION", didClose: () => {comision.focus()}}); return};    
     
     const cuenta = document.getElementById("cuenta");
     const cargo = document.getElementById("cargo");
@@ -62,9 +62,32 @@ function EnviarDatos(data) {
         body: JSON.stringify(data),    
     })            
     .then((response) => response.json())
-    .then((data) => {     
-        if (data.success) {
-            EnviarImgs();
+    .then((result) => {     
+        if (result.success) {
+            dni = data.dni          
+            document.getElementById("dni").value = "";
+            document.getElementById("paterno").value = "";
+            document.getElementById("materno").value = "";
+            document.getElementById("nombre").value = ""; 
+            document.getElementById("nacimiento").value = "";
+            document.getElementById("ingreso").value = "";
+            document.getElementById("planilla").value = "";
+            document.getElementById("movilidad").value = "";
+            document.getElementById("asignacion").value = ""; 
+            document.getElementById("aportacion").value = "";
+            document.getElementById("comision").value = "";
+            document.getElementById("cuenta").value = "";
+            document.getElementById("cargo").value = "";
+            document.getElementById("distrito").value = "";
+            document.getElementById("domicilio").value = "";
+            document.getElementById("area").value = "";
+            document.getElementById("cuspp").value = "";
+            document.getElementById("celular").value = "";
+            document.getElementById("licencia").value = "";
+            document.getElementById("categoria").value = "";
+            document.getElementById("revalidacion").value = "";     
+            document.getElementById("dni").focus();     
+            EnviarImgs(dni);            
         };        
     })
     .catch((error) => {
@@ -72,7 +95,7 @@ function EnviarDatos(data) {
     });
 };
 
-function EnviarImgs() {
+function EnviarImgs(dni) {
     const fileFoto = document.getElementById("file-foto");
     const fileDni = document.getElementById("file-dni");
     const fileLicencia = document.getElementById("file-licencia");
@@ -81,26 +104,38 @@ function EnviarImgs() {
     if (fileFoto.value || fileDni.value || fileLicencia.value || filePolicial.value) {
         const newFormData = new FormData();
 
-        if (fileFoto.value){newFormData.append("files", fileFoto.files[0], "foto.jpg")};
-        if (fileDni.value){newFormData.append("files", fileDni.files[0], "dni.jpg")};
-        if (fileLicencia.value){newFormData.append("files", fileLicencia.files[0], "licencia.jpg")};
-        if (filePolicial.value){newFormData.append("files", filePolicial.files[0], "policial.jpg")};
+        if (fileFoto.value){newFormData.append("files", fileFoto.files[0], dni + "-foto.jpg")};
+        if (fileDni.value){newFormData.append("files", fileDni.files[0], dni + "-dni.jpg")};
+        if (fileLicencia.value){newFormData.append("files", fileLicencia.files[0], dni + "-licencia.jpg")};
+        if (filePolicial.value){newFormData.append("files", filePolicial.files[0], dni + "-policial.jpg")};
 
         fetch('/uploadfiles', {
             method: 'POST',        
             body: newFormData
         })
-        .then(res => res.json())
-        .then(res => {
-            console.log(res);                   
+        .then(response => response.json())
+        .then(result => {          
+            if (result.success) {                
+                document.getElementById("file-foto").value = "";
+                document.getElementById("file-dni").value = ""; 
+                document.getElementById("file-licencia").value = "";
+                document.getElementById("file-policial").value = "";    
+            }                       
         })
         .catch(err => {
             console.log(err);
         });
-    };       
+    }; 
+    
+    Swal.fire({        
+        icon: 'success',
+        title: 'Los Datos Fueron Guardados',
+        showConfirmButton: false,
+        timer: 1200
+      });
 };
 
-function ComisionValid(event) {
+function ComisionValid() {
     const aportacion = document.getElementById("aportacion");
     const comision = document.getElementById("comision");
     if (comision.value && (aportacion.value == "onp" || aportacion.value == "")){        
@@ -109,7 +144,7 @@ function ComisionValid(event) {
     };
 };
 
-function AporteChange(event) {
+function AporteChange() {
     const aportacion = document.getElementById("aportacion");
     const comision = document.getElementById("comision");
     if (!aportacion.value || aportacion.value == "onp"){        

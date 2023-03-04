fc04e7e8ca73e6eec2665ba275f0f7ef738a9955
Jean <jeanoi4212@gmail.comm>
2023-03-04 09:01:56 -0500

    mejorando

--- static/asistencia-click.js
+++ static/asistencia-click.js
@@ -1,4 +1,36 @@
-var myElementToCheckIfClicksAreInsideOf = document.querySelector('.item');
+const myFunction = (e) => {
+    
+    let reviewNode = e.target;
+    
+    while (reviewNode.nodeName !== 'BODY') {
+    
+      if (reviewNode.classList.contains('item')) {
+        Swal.fire({
+            title: 'Do you want to save the changes?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Apoyo',
+            denyButtonText: `Falta`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire('Saved!', '', 'success');
+                e.target.innerText = "X";
+            } else if (result.isDenied) {
+                Swal.fire('Saved!', '', 'success');
+                e.target.innerText = "&check;";
+            }
+        })
+        break;
+      }      
+      reviewNode = reviewNode.parentNode;
+    }      
+  }
+  
+  document.body.addEventListener('click', myFunction, false);
+
+
+/*var myElementToCheckIfClicksAreInsideOf = document.querySelector('.item');
 // Listen for click events on body
 document.body.addEventListener('click', function (event) {
     let id = event.target.id
@@ -38,4 +70,4 @@ document.body.addEventListener('click', function (event) {
         }
     });    
 });
-
+*/

321364e1e6d6ebc10409a6ed77cd175c59d1682d
jeancarlos <jeanoi4212@gmail.com>
2023-05-03 08:50:21 -0500

    mejorandp

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from routers.obtenerUser import ruta
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-origins = ["http://localhost", "http://localhost:8080", "http://localhost:3000"]
+origins = ["http://localhost:8080"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.exception_handler(exc_class_or_status_code=404)
--- routers/obtenerUser.py
+++ routers/obtenerUser.py
@@ -1,5 +1,6 @@
 from fastapi import	APIRouter
 from pydantic import BaseModel
+from models.model import Users, session
 
 class User(BaseModel):
     username: str
@@ -9,5 +10,17 @@ ruta = APIRouter()
 
 @ruta.post("/login")
 def login_user(user: User):
-    print(user.username)
-    print(user.password)
+
+    exist_user = session.query(Users).filter(Users.username == user.username).all()
+        
+    if exist_user:
+
+        if exist_user[0].password == user.password:                    
+            return {"message": "ok"}
+            
+        return {"message": "invalid password"}
+        
+    return {"message": "invalid username"}
+    
+    #print(user.username)
+    #print(user.password)
